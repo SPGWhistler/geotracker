001cc81566980af718917e6ac665ffe0166bf7fe
Tony Petty <tpetty@appmobi.com>
2013-06-12 23:36:06 -0400

    Got simple tracking working.

--- server/index.php
+++ server/index.php
@@ -1,7 +1,18 @@
 <?php
-$coords = array(
-	'lat' => '43.01168',
-	'lng' => '-78.34136',
-);
-echo json_encode($coords);
+$output = array();
+if (isset($_GET['uuid'])) {
+	if (isset($_GET['lat']) && isset($_GET['lng'])) {
+		if (stripos($_GET['uuid'], 'emu') !== FALSE) {
+			file_put_contents('tmp', $_GET['lat'] . "," . $_GET['lng']);
+		}
+		$output = array("success" => TRUE);
+	} else {
+		$data = file_get_contents('tmp');
+		$output = array(
+			'lat' => substr($data, 0, strpos($data, ",")),
+			'lng' => substr($data, strpos($data, ",") + 1),
+		);
+	}
+}
+echo json_encode($output);
 ?>
