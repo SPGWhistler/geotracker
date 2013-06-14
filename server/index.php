b7e1bb328bddbb73d8bd67bd7216c098296d729e
Tony Petty <tpetty@appmobi.com>
2013-06-13 23:32:21 -0400

    Got tracking working.

--- server/index.php
+++ server/index.php
@@ -1,17 +1,32 @@
 <?php
 $output = array();
 if (isset($_GET['uuid'])) {
+	$mongo = new MongoClient();
+	$db = $mongo->locations;
+	$collection = new MongoCollection($db, 'locations');
 	if (isset($_GET['lat']) && isset($_GET['lng'])) {
-		if (stripos($_GET['uuid'], 'emu') !== FALSE) {
-			file_put_contents('tmp', $_GET['lat'] . "," . $_GET['lng']);
-		}
+		$collection->update(
+			array('uuid' => $_GET['uuid']),
+			array(
+				'uuid' => $_GET['uuid'],
+				'lat' => $_GET['lat'],
+				'lng' => $_GET['lng'],
+				'ts' => time()
+			),
+			array('upsert' => TRUE)
+		);
 		$output = array("success" => TRUE);
 	} else {
-		$data = file_get_contents('tmp');
-		$output = array(
-			'lat' => substr($data, 0, strpos($data, ",")),
-			'lng' => substr($data, strpos($data, ",") + 1),
-		);
+		$query = array('uuid' => $_GET['uuid']);
+		$cursor = $collection->find($query);
+		$obj = $cursor->getNext();
+		if (isset($obj)) {
+			$output = array(
+				'lat' => $obj['lat'],
+				'lng' => $obj['lng'],
+				'ts' => $obj['ts'],
+			);
+		}
 	}
 }
 echo json_encode($output);
